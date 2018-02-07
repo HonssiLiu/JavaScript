f2b58f2bc18655b6e74cd47b0520e834f9962564
liuhongxing <liuhongxing@hnwdkj.com>
2018-02-07 17:30:48 +0800

    00

---
+++ src/main/java/ikanalyzer/Demo.java
@@ -0,0 +1,18 @@
+package ikanalyzer;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+
+public class Demo {
+	public static void main(String[] args) throws IOException {
+		String text = "中南大学铁道学院";
+		IKSegmenter ik = new IKSegmenter(new StringReader(text), true);
+		Lexeme lexeme = null;
+		while ((lexeme = ik.next()) != null) {
+			System.out.println(lexeme.getLexemeText());
+		}
+	}
+}
