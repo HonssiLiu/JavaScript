f9c148ccb677e929ffce4e0cea917a9b9982b87d
liuhongxing <liuhongxing@hnwdkj.com>
2018-02-07 17:34:23 +0800

    ..

--- src/main/java/ikanalyzer/Demo.java
+++ src/main/java/ikanalyzer/Demo.java
@@ -8,7 +8,7 @@ import org.wltea.analyzer.core.Lexeme;
 
 public class Demo {
 	public static void main(String[] args) throws IOException {
-		String text = "中南大学铁道学院";
+		String text = "中南大学铁道学院,420000,湖南长沙，*&@#$school of";
 		IKSegmenter ik = new IKSegmenter(new StringReader(text), true);
 		Lexeme lexeme = null;
 		while ((lexeme = ik.next()) != null) {
